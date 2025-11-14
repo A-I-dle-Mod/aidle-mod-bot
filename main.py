9d35063f4eeaff8386b1dc1c6aac1c3d0f0aa2ac
AndrewLowther <randomsite@gmail.com>
2025-11-14 08:18:31 +0000

    Initial bot commit
    Fairly basic as it stands, requests the moderate API with
    message data and does not return any information as of yet

---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import discord
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents(messages=True, message_content=True, guilds=True, members=True)
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  print(f'Logged in as {client.user}')
+
+@client.event
+async def on_guild_join(guild):
+  print(f'Joined guild: {guild.name} (id: {guild.id})')
+
+  r = requests.post(
+    os.getenv('API_URL') + '/guild',
+    json={
+      "owner_id": guild.owner_id,
+      "owner_name": guild.owner.name,
+      "owner_icon": str(guild.owner.avatar.url) if guild.owner.avatar else None,
+      "guild_name": guild.name,
+      "guild_id": guild.id,
+      "guild_icon": str(guild.icon.url) if guild.icon else None,
+      "moderate": True
+    }
+  )
+
+  print('Guild registration response:', r.json())
+
+@client.event
+async def on_guild_remove(guild):
+  print("Got remove guild request")
+
+  r = requests.delete(
+    os.getenv('API_URL') + '/guild/' + str(guild.id)
+  )
+
+  print(f'Removed from guild: {guild.name} (id: {guild.id})')
+
+@client.event
+async def on_message(message):
+  print(f'Message from {message.author}: {message.content}')
+
+  r = requests.post(
+    os.getenv('API_URL') + '/moderate',
+    json={
+      "input_text": message.content,
+      'metadata': {
+        'message_id': message.id,
+        'author_id': message.author.id,
+        'author_name': message.author.global_name,
+        'guild_id': message.guild.id
+      }
+    }
+  )
+
+  print('Moderation results:', r.json())
+
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('!hello'):
+    await message.channel.send('Hello! How can I assist you today?')
+
+client.run(os.getenv('BOT_TOKEN'))
